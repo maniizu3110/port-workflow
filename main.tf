0613ed6f92205b5f69b12b6468cb87005ddf6fc9
maniizu3110 <maniizu3110@gmail.com>
2023-04-23 20:24:07 +0900

    set local

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "examples" {
+resource "aws_s3_bucket" "portexamples" {
   bucket = var.bucket_name
   acl    = "private"
 }
