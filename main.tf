a697c2d9405508d08598e872d75ca5d847860d96
maniizu3110 <maniizu3110@gmail.com>
2023-04-23 19:38:57 +0900

    change bucket name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "port-example" {
-  bucket = "port-example-bucket"
+resource "aws_s3_bucket" "port-example2" {
+  bucket = "port-example-bucket2"
   acl    = "private"
 }
