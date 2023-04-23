6da7401bfc6e9d242f5544d282e3d62e5ec244bd
maniizu3110 <maniizu3110@gmail.com>
2023-04-23 20:15:20 +0900

    set local

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
-locals {
-  bucket_name = "${getenv("BUCKET_NAME") != "" ? getenv("BUCKET_NAME") : "default-bucket-name"}"
-}
+
 
 provider "aws" {
   region = "ap-northeast-1"
 }
 
 resource "aws_s3_bucket" "examples" {
-  bucket = local.bucket_name
+  bucket = var.bucket_name
   acl    = "private"
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
+  default     = "my-bucket"
 }
