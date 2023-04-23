4396ff548fddbac0dea8fb8c4e878e8da5cf1245
maniizu3110 <maniizu3110@gmail.com>
2023-04-23 20:07:17 +0900

    set local

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 locals {
-  s3_bucket_name = "${getenv("BUCKET_NAME") != "" ? getenv("BUCKET_NAME") : "default-bucket-name"}"
+  bucket_name = "${getenv("BUCKET_NAME") != "" ? getenv("BUCKET_NAME") : "default-bucket-name"}"
 }
 
 provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "examples" {
+  bucket = local.bucket_name
   acl    = "private"
 }
